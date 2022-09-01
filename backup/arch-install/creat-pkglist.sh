28f470fc8abed7dcfd27998d18003d73f76e196b
goodaniu <goodaniu@163.com>
2022-09-01 21:26:19 +0800

    清除所有文件

--- backup/arch-install/creat-pkglist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#此脚本在完好系统中调用并生成软件列表备份
-comm -23 <(pacman -Qeq|sort) <(pacman -Qgq base base-devel|sort)  >pkglist
-comm -12 <(comm -23 <(pacman -Qeq|sort) <(pacman -Qmq|sort)) <(sort pkglist) >paclist
-comm -13 <(comm -23 <(pacman -Qeq|sort) <(pacman -Qmq|sort)) <(sort pkglist) >aurlist
-comm -12 <(comm -23 <(pacman -Qdq|sort) <(pacman -Qmq|sort)) <(sort pkglist) >asdepslist
